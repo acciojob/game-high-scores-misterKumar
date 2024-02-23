38d5cea2f937f9588b60acca7ac833db5306cdbd
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-02-23 20:18:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,31 +3,78 @@ const nameInput = document.getElementById("name");
 const scoreInput = document.getElementById("score");
 const scores = document.getElementById("scores");
 
-
-const table = document.createElement("table");
-const tr = document.createElement("tr");
-tr.innerHTML = `<th>Name</th><th>Score</th>`
-table.append(tr);
 // Save score to Local Storage
 function saveScore() {
-  // complete the code here
-	scores.append(table);
-    let nameInput = document.getElementById("name").value;
-    let scoreInput = document.getElementById("score").value;
-    if(scoreInput==""){
-        scoreInput= "No scores yet";
-    }
-    let tr = document.createElement("tr");
-    tr.innerHTML= `<td>${nameInput}</td><td>${scoreInput}</td>`
-    table.appendChild(tr); 
-    localStorage.setItem("table",JSON.stringify(table));
+// complete the code here
 
+	let name = nameInput.value;
+	let score = scoreInput.value;
+
+	let scoreArr = [];
+
+	let value = localStorage.getItem("scores");
+	
+	if(value !== null){
+		   scoreArr = JSON.parse(value)
+		   scoreArr.push({name:name, score: parseInt(score)})
+		   localStorage.setItem("scores", JSON.stringify(scoreArr));
+	 }
+	else{
+		scoreArr.push({name:name, score: parseInt(score)})
+		localStorage.setItem("scores", JSON.stringify(scoreArr));
+	}
+	
   showScores();
 }
 
 // Show scores in div
 function showScores() {
   // complete the code
-	let tableData = JSON.parse(localStorage.getItem("table")) 
-    // scores.innerText = table 	
+     scores.innerHTML = "";  // clear the prevoius data 
+    //localStorage.removeItem("scores")  // testing perpose
+	
+	
+	let objArr = JSON.parse(localStorage.getItem("scores"));
+
+	if(objArr === null){
+		scores.innerHTML = "No scores yet";
+	}
+	else{
+		objArr.sort((x,y)=>{
+			if(x.score < y.score){
+				return 1;
+			}
+			if(x.score > y.score){
+				return -1;
+			}
+			return 0;
+		})
+         
+		let ta = document.createElement("table");
+		     ta.id = "scores table tr";
+		
+		let th1 = document.createElement("th");
+		     th1.innerHTML = "Name"
+		let th2 = document.createElement("th");
+		     th2.innerHTML = "Score"
+		let tr1 = document.createElement("tr");
+		     tr1.append(th1,th2);
+		     ta.append(tr1)
+		
+
+		objArr.map(({name, score})=>{
+			 let td1 = document.createElement("td");
+				  td1.innerHTML = name;
+			 let td2 = document.createElement("td");
+				  td2.innerHTML = score
+			 let tr2 = document.createElement("tr");
+				  tr2.append(td1,td2);
+			
+		   ta.append(tr2)  
+		})
+
+		    
+	  scores.append(ta)   // add the whole table to score div	
+	}
+	
 }
